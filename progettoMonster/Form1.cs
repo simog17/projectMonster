9b7cf7f5e4b1b1c006b35f9dc502a015a32211f9
simog17 <simogaddi17@gmail.com>
2016-12-01 18:38:29 +0100

    First commit

---
+++ progettoMonster/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progettoMonster
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Monster strongbonna = new Monster("Stongbonna", 10, 50);
+            Monster charmender = new Monster("charmender", 56, 10);
+
+            strongbonna.attack(charmender);
+            strongbonna.attack(charmender);
+            strongbonna.attack(charmender);
+        }
+    }
+}
---
+++ progettoMonster/Monster.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progettoMonster
+{
+    class Monster
+    {
+        public string name;
+        public int maxHp;
+        public int curHp;
+        public int damage;
+
+        public Monster(string name, int maxHp, int damage)
+        {
+            this.name = name;
+            this.maxHp = maxHp;
+            curHp = maxHp;
+            this.damage = damage;
+            describe();
+        }
+
+        public string describe()
+        {
+            string output = "questo è " + name + "\r\n";
+            output += "HP: " + curHp + "\r\n";
+            output += "DAMAGE: " + damage + "\r\n";
+
+            return output;
+        }
+
+        public void describe(TextBox t)
+        {
+            t.Text = describe();
+        }
+
+        public void attack(Monster target)
+        {
+            if (target.curHp <= 0)
+            {
+                Console.WriteLine(target.name + " è già esausto, non infierire.");
+                return;
+            }
+
+            Console.WriteLine(name + " attacca " + target.name);
+            Console.WriteLine(name + " fa " + damage + " danni a " + target.name);
+            target.curHp -= damage;
+
+            if (target.curHp <= 0)
+            {
+                target.curHp = 0;
+                Console.WriteLine(target.name + " è esausto.");
+            }
+            else
+            {
+                Console.WriteLine("a " + target.name + " rimangono " + target.curHp + " hp");
+            }
+        }
+    }
+}
